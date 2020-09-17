4db659f82672f9c62be308f82c5186ec5b3b8782
= <=>
2020-09-17 20:15:45 -0300

    :recycle: update connection object on user repository

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,11 +1,11 @@
-import { Connection } from 'typeorm'
 import User from '@entities/User';
 import { CreateUserRequestDTO } from '@useCases/users/createUser/createUserDTO';
+import { Database } from '@config/database'
 
 export default class UserRepository {
-  constructor(private connection: Promise<Connection>) { }
+  constructor(private database: Database) { }
   async createUser(createUserRequestDTO: CreateUserRequestDTO): Promise<User> {
-    const repository = (await this.connection).getRepository(User);
+    const repository = (await this.database.getConnection()).getRepository(User);
     const { name, email, password } = createUserRequestDTO;
     const user = new User();
     user.name = name;
