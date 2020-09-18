359261916f78dadb8fc8085471a600ba898ec7f9
= <=>
2020-09-18 18:04:58 -0300

    :construction: validate createUser route

--- src/useCases/users/createUser/createUserController.ts
+++ src/useCases/users/createUser/createUserController.ts
@@ -1,10 +1,17 @@
+import { ValidationException } from '@errors/ValidationException';
 import { NextFunction, Request, Response } from 'express';
+import { validationResult } from 'express-validator';
 import CreateUserUseCase from './createUserUseCase';
 
 export default class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) { }
 
   execute = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+    const validationErrors = validationResult(request);
+    if (!validationErrors.isEmpty()) {
+      return next(new ValidationException());
+    }
+
     const body = request.body
     try {
       const user = await this.createUserUseCase.execute(body);
