12cd06e91112180909302be50dbc6d8be4d26c3d
= <=>
2020-09-19 22:09:54 -0300

    :art: condigure database with env variables

--- src/config/database/index.ts
+++ src/config/database/index.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { createConnection, Connection } from 'typeorm'
 
 export class Database {
@@ -8,7 +9,17 @@ export class Database {
   }
 
   private async init () {
-    this.connection = await createConnection()
+    this.connection = await createConnection({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: 5432,
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.DATABASE,
+      synchronize: true,
+      logging: false,
+      entities: [path.resolve(__dirname, '../../entities/*{.ts,.js}')]
+    })
   }
 
   async getConnection (): Promise<Connection> {
