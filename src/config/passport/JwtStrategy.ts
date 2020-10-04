70a582b64130673989c986f1809ff1dc32796a85
= <=>
2020-10-04 18:24:17 -0300

    :construction: configure jwtstrategy

--- src/config/passport/JwtStrategy.ts
+++ src/config/passport/JwtStrategy.ts
@@ -3,7 +3,7 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
 export default new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.APP_SECRET
-}, (payload, done) => {
-  if (!payload) return done(null, false)
+}, async (payload, done) => {
+  if (!payload) { return done(null, false) }
   done(null, payload)
 })
