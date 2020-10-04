b0c39c77fe5be49b869e7c629ffbe159cf94834c
= <=>
2020-10-04 18:38:54 -0300

    :sparkles: create product service

--- src/services/products/createProduct/createProductController.ts
+++ src/services/products/createProduct/createProductController.ts
@@ -1,16 +1,16 @@
 import requestErrorHandler from '@utils/requestErrorHandler'
 import { NextFunction, Request, Response } from 'express'
-import AuthUserLocalService from './authUserLocalService'
+import CreateProductService from './createProductService'
 
-export default class AuthUserLocalController {
-  constructor (private authUserLocalService: AuthUserLocalService) { }
-
+export default class CreateProductController {
+  constructor (private createProductService: CreateProductService) {}
   execute = async (request: Request, response: Response, next: NextFunction): Promise<void> => {
     if (!requestErrorHandler(request, next)) return
     const body = request.body
+    const { id } = request.user
     try {
-      const user = await this.authUserLocalService.execute(body)
-      response.status(200).json(user)
+      const product = await this.createProductService.execute(body, id)
+      response.status(200).json(product)
     } catch (error) {
       next(error)
     }
---
+++ src/services/products/createProduct/createProductDTO.ts
@@ -0,0 +1,9 @@
+export interface CreateProductRequestDTO {
+  name: string,
+  description: string
+}
+
+export interface CreateProductResponseDTO {
+  name: string,
+  description: string
+}
